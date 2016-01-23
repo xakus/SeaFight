55539a14a163bdb14d454222e6bb8dced6c8d4aa
xakus <xakus22@gmail.com>
2016-01-23 15:12:37 +0400

    AI class demey olarki hazirdi amma test elemey lazimdi

--- src/com/XAKUS/ComputerAI/AI.java
+++ src/com/XAKUS/ComputerAI/AI.java
@@ -93,7 +93,7 @@ public class AI {
 
                   }
                   try {
-                        Thread.sleep(2000);
+                        Thread.sleep(1000);
                   } catch(InterruptedException e) {
                         e.printStackTrace();
                   }
